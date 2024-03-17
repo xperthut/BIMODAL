fc6bdbeab30a9851605fa1da54232f55d64902eb
xperthut <mhmethun@gmail.com>
2024-03-17 02:36:42 -0700

    new

--- generate_smile.sh
+++ generate_smile.sh
@@ -6,6 +6,6 @@ CONDA_BASE_DIR=~/software/Miniconda3
 # Activate the conda environment.
 source "$CONDA_BASE_DIR/etc/profile.d/conda.sh"
 
-source activate Torch
+source activate Torch-CUDA-11
 
 python model/generate.py --modelname $1 --runtype $2 --TotalMol $3
